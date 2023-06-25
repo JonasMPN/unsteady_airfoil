f86be0c1547cb31f65c646ce26d7c2111685d56a
Jonas MPN <you@example.com>
2023-06-25 13:15:46 +0200

    Small adaptions

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -8,10 +8,9 @@ test = {
 	"cp_induction": False,
 	"free_vortex_induction": False,
 	"lhs_matrix": False,
-	"without_free": True,
-	"with_free": False
+	"without_free": False,
+	"with_free": True
 }
-
 
 if test["displacing"]:
 	time_steps = 10
@@ -26,9 +25,8 @@ if test["displacing"]:
 		if i == time_steps-1:
 			continue
 		geom.displace_vortices(np.c_[np.ones(i+3), np.zeros(i+3)], dt)
-	geom.plot()
-	
-	
+	geom.plot_final_state()
+
 if test["cp_induction"]:
 	time_steps = 10
 	dt = 0.1
@@ -40,16 +38,16 @@ if test["cp_induction"]:
 	geom.update_rotation(0, 0)
 	geom.shed_vortex((1, 0), time_step=0.5)
 	geom.add_free_vortices(np.asarray([[3, 1]]))
-	
+
 	ind = Induction(blade_res, flap_res)
-	
+
 	bound, control, trailing, free = geom.get_positions()
 	print("Inversely calculated distances from the free vortices to the control points:")
 	print(1/(2*np.pi*ind.control_point_induction(plate_control_points=control["plate"],
-	                                             vortices=np.r_[trailing, free],
-	                                             flap_control_points=control["flap"])))
-	geom.plot(ls_trailing="x")
-	
+												 vortices=np.r_[trailing, free],
+												 flap_control_points=control["flap"])))
+	geom.plot_final_state(ls_trailing="x")
+
 if test["free_vortex_induction"]:
 	time_steps = 10
 	dt = 0.1
@@ -60,12 +58,12 @@ if test["free_vortex_induction"]:
 	geom.set_flap(1, flap_res)
 	geom.update_rotation(0, 0)
 	geom.shed_vortex((1, 0), time_step=0.5)
-	geom.displace_vortices(np.asarray([[1, 0]]), time_step=0.5,)
+	geom.displace_vortices(np.asarray([[1, 0]]), time_step=0.5, )
 	geom.shed_vortex((1, 0), time_step=0.5)
 	geom.add_free_vortices(np.asarray([[2.25, 1]]))
-	
+
 	ind = Induction(blade_res, flap_res)
-	
+
 	bound, control, trailing, free = geom.get_positions()
 	bound = np.r_[bound["plate"], bound["flap"]]
 	from_trailing, from_bound = ind.free_vortex_induction(np.r_[trailing, free], bound)
@@ -79,7 +77,7 @@ if test["free_vortex_induction"]:
 	print(from_bound["x"])
 	print("y")
 	print(from_bound["y"])
-	geom.plot()
+	geom.plot_final_state()
 
 if test["lhs_matrix"]:
 	time_steps = 10
@@ -91,12 +89,12 @@ if test["lhs_matrix"]:
 	geom.set_flap(1, flap_res)
 	geom.update_rotation(0, 0)
 	geom.shed_vortex((1, 0), time_step=0.5)
-	
+
 	ind = Induction(blade_res, flap_res)
 	bound, control, trailing, free = geom.get_positions()
 	bound = np.r_[bound["plate"], bound["flap"]]
 	print(ind.lhs_matrix(bound, trailing[-1], control["plate"], control["flap"]))
-	geom.plot(ls_trailing="x")
+	geom.plot_final_state(ls_trailing="x")
 
 if test["without_free"]:
 	time_steps = 800
@@ -110,9 +108,9 @@ if test["without_free"]:
 	plate_angles[10:] = -10
 	inflow = (1, 0)
 	unsteady_airfoil.solve(dt=dt, plate_angles=plate_angles, inflows=inflow, flap_angles=0.0)
-	
+
 if test["with_free"]:
-	time_steps = 250
+	time_steps = 200
 	dt = 0.005
 	plate_res = 1
 	flap_res = 1
@@ -121,6 +119,7 @@ if test["with_free"]:
 	unsteady_airfoil = UnsteadyAirfoil(time_steps, plate_res, plate_length, flap_res, flap_length)
 	unsteady_airfoil.add_free_vortices(np.asarray([[0., 10.]]), 0.1)
 	plate_angles = np.zeros(time_steps)
-	plate_angles[:] = -10
+	# plate_angles[:] = -10
 	inflow = (1, 0)
 	unsteady_airfoil.solve(dt=dt, plate_angles=plate_angles, inflows=inflow, flap_angles=0.0)
+	unsteady_airfoil.plot_final_state()
--- src/unsteady_aero.py
+++ src/unsteady_aero.py
@@ -5,8 +5,8 @@ import numpy as np
 
 class UnsteadyAirfoil:
 	def __init__(self, max_time_steps: int,
-	             plate_res: int, plate_length: float,
-	             flap_res: int = None, flap_length: float = None):
+				 plate_res: int, plate_length: float,
+				 flap_res: int = None, flap_length: float = None):
 		self.plate_res = plate_res
 		self.flap_res = flap_res
 		self.n_time_steps = max_time_steps
@@ -15,30 +15,30 @@ class UnsteadyAirfoil:
 		self.geometry = Geometry(max_time_steps)
 		self.induction = Induction(plate_res, flap_res)
 		self.n_free_vortices = 0
-		
+
 		self.bound_circulation = {"plate": np.zeros(max_time_steps), "flap": np.zeros(max_time_steps)}
-		
+
 		self.geometry.set_plate(plate_length, plate_res)
 		if flap_res is not None:
 			self.geometry.set_flap(flap_length, flap_res)
 			self.use_flap = True
-	
+
 	def toggle_flap(self, use_flap: bool) -> None:
 		self.use_flap = use_flap
 		return None
-	
+
 	def add_free_vortices(self, coordinates: np.ndarray, circulations: list or np.ndarray) -> None:
 		self.geometry.add_free_vortices(coordinates)
 		self.free_vortices_circulation = circulations if type(circulations) == np.ndarray else np.asarray(circulations)
 		self.n_free_vortices = self.free_vortices_circulation.size
 		return None
-		
+
 	def solve(self,
-	          dt: float,
-	          plate_angles: float or list or np.ndarray,
-	          inflows: tuple or list[list] or np.ndarray,
-	          flap_angles: float or list or np.ndarray = None,
-	          shed_trailing_distance: float = 0.5) -> dict[str, np.ndarray]:
+			  dt: float,
+			  plate_angles: float or list or np.ndarray,
+			  inflows: tuple or list[list] or np.ndarray,
+			  flap_angles: float or list or np.ndarray = None,
+			  shed_trailing_distance: float = 0.5) -> dict[str, np.ndarray]:
 		plate_angles = plate_angles if type(plate_angles) != float else [plate_angles for _ in range(self.n_time_steps)]
 		inflows = inflows if type(inflows) != tuple else [[inflows[0], inflows[1]] for _ in range(self.n_time_steps)]
 		if flap_angles is None:
@@ -46,7 +46,7 @@ class UnsteadyAirfoil:
 		else:
 			flap_angles = flap_angles if type(flap_angles) != float else [flap_angles for _ in range(self.n_time_steps)]
 		self.geometry.toggle_flap(self.use_flap)
-		
+
 		surrounding_vortices = np.zeros((self.n_free_vortices+self.n_time_steps, 2))
 		surrounding_vortices[:self.n_free_vortices] = self.geometry.get_positions()[3]
 		surrounding_circulation = np.zeros((self.n_free_vortices+self.n_time_steps, 1))
@@ -63,63 +63,51 @@ class UnsteadyAirfoil:
 				bound, control, trailing, free = self.geometry.get_positions()
 				combined_bound = np.r_[bound["plate"], bound["flap"]]
 				lhs = self.induction.lhs_matrix(bound_vortices=combined_bound, shed_vortex=trailing[-1, :],
-				                                plate_control_points=control["plate"],
-				                                flap_control_points=control["flap"])
+												plate_control_points=control["plate"],
+												flap_control_points=control["flap"])
 				inv_lhs = np.linalg.inv(lhs)
 				plate_normal, flap_normal = self.geometry.get_normals()
 				plate_inflow = np.asarray([inflow[0], inflow[1]])@plate_normal
 				flap_inflow = np.asarray([inflow[0], inflow[1]])@flap_normal
-				normal_inflow = np.r_[plate_inflow*np.ones((self.plate_res, 1)), flap_inflow*np.ones((self.flap_res, 1))]
+				normal_inflow = np.r_[
+					plate_inflow*np.ones((self.plate_res, 1)), flap_inflow*np.ones((self.flap_res, 1))]
 				old_plate_angle, old_flap_angle, old_inflow = plate_angle, flap_angle, inflow
 			else:
 				self.geometry.shed_vortex(inflow=inflow, time_step=dt, new_trailing_fac=shed_trailing_distance)
 				bound, control, trailing, free = self.geometry.get_positions()
-			
+
 			cpi = self.induction.control_point_induction(plate_control_points=control["plate"],
-			                                             flap_control_points=control["flap"],
-			                                             vortices=surrounding_vortices[:dt_i])
+														 flap_control_points=control["flap"],
+														 vortices=surrounding_vortices[:dt_i])
 			induction_from_surrounding = cpi@surrounding_circulation[:dt_i]
 			bound_circulation = -inv_lhs@(np.r_[normal_inflow+induction_from_surrounding,
-			                                   [[np.sum(surrounding_circulation[self.n_free_vortices:dt_i]), ]]])
+												[[np.sum(surrounding_circulation[self.n_free_vortices:dt_i]), ]]])
 			surrounding_circulation[dt_i, 0] = bound_circulation[-1]
 
 			surrounding_vortices = np.r_[free, trailing]
 			from_surrounding, from_bound = self.induction.free_vortex_induction(free_vortices=surrounding_vortices,
-			                                                                    bound_vortices=combined_bound)
-			
+																				bound_vortices=combined_bound)
+
 			from_wake_velocities_x = from_surrounding["x"]@surrounding_circulation[:dt_i+1]
 			from_wake_velocities_y = from_surrounding["y"]@surrounding_circulation[:dt_i+1]
 			from_bound_velocities_x = from_bound["x"]@bound_circulation[:-1]
 			from_bound_velocities_y = from_bound["y"]@bound_circulation[:-1]
-			
+
 			x_vel = from_wake_velocities_x+from_bound_velocities_x
 			y_vel = from_wake_velocities_y+from_bound_velocities_y
 			background_flow = np.asarray((dt_i+1)*[[inflow[0], inflow[1]]])
 			self.geometry.displace_vortices(velocities=np.c_[x_vel, y_vel]+background_flow, time_step=dt)
 			self.bound_circulation["plate"][dt_i-self.n_free_vortices] = np.sum(bound_circulation[:self.plate_res])
 			self.bound_circulation["flap"][dt_i-self.n_free_vortices] = np.sum(bound_circulation[self.plate_res:])
-			if (dt_i-self.n_free_vortices) % 20 == 0:
+			if (dt_i-self.n_free_vortices)%20 == 0:
 				print("iteration: ", dt_i-self.n_free_vortices, "bound circulation: ", np.sum(bound_circulation[:-1]))
 		return self.bound_circulation
-	
-		
-	
-			
-			
-		
-			
-			
-			
-			
-			
-		
-		
-		
-		
-		
 
-
-
-		
-		
-		
+	def plot_final_state(self,
+						 show=True,
+						 plot_structure: bool = True,
+						 ls_bound: str = "o",
+						 ls_control: str = "x",
+						 ls_trailing: str = "-",
+						 ls_free: str = "x") -> None or tuple:
+		return self.geometry.plot_final_state(show, plot_structure, ls_bound, ls_control, ls_trailing, ls_free)
